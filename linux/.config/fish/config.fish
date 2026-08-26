4ef43043911b588da6acedced4f681dedc676d47
Alejandro Hernandez <azure.satellite@gmail.com>
2026-08-26 15:36:10 -0400

    Multi-arch fixes

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,5 +1,6 @@
 status is-interactive; and begin
     set fish_greeting
+    mise activate fish | source
     starship init fish | source
     zoxide init fish | source
 end
--- macos/.config/fish/config.fish
+++ macos/.config/fish/config.fish
@@ -1,10 +1,7 @@
 status is-interactive; and begin
-    set fish_greeting # Disable the fish greeting
-    switch (uname)
-        case Darwin
-            ~/.local/bin/mise activate fish | source
-            /opt/homebrew/bin/brew shellenv fish | source
-    end
+    set fish_greeting
+    ~/.local/bin/mise activate fish | source
+    /opt/homebrew/bin/brew shellenv fish | source
     starship init fish | source
     zoxide init fish | source
 end
